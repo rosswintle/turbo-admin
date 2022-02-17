d4f79fad73796cd324da0be2a63f00c13cc462b4
Ross Wintle <rosswintle@users.noreply.github.com>
2022-02-17 19:37:10 +0000

    Update more files from the extension

--- src/class-turbo-admin-wp-block-editor-welcome-guide-kill.js
+++ src/class-turbo-admin-wp-block-editor-welcome-guide-kill.js
@@ -4,17 +4,47 @@
 export default class TurboAdminWpBlockEditorWelcomeGuideKill {
 
     constructor() {
-        // This needs a slight delay as it needs to happen after the editor
-        // is initialised
-        setTimeout(() => {
-            const welcomeGuide = document.querySelector('.edit-post-welcome-guide');
-            if (welcomeGuide) {
-                const closeButton = welcomeGuide.querySelector('.components-modal__header button');
-                if (closeButton) {
-                    closeButton.click();
+
+        const attrObserver = new MutationObserver((mutations) => {
+            mutations.forEach(mu => {
+                // Check if we already killed the modal
+                if (document.body.classList.contains('ta-killed-post-welcome-guide')) {
+                    return;
                 }
-            }
-        }, 400);
+
+                if (mu.type !== "attributes" && mu.attributeName !== "class") {
+                    return;
+                }
+
+                if (! mu.target.classList.contains('modal-open')) {
+                    return;
+                }
+
+                const welcomeGuide = document.querySelector('.edit-post-welcome-guide');
+                if (welcomeGuide) {
+                    this.killWelcomeGuide();
+                }
+            });
+        });
+
+        attrObserver.observe(document.body, { attributes: true });
+
+        // Also attempt a kill now in case it's already appeared.
+        this.killWelcomeGuide();
     }
 
+    killWelcomeGuide() {
+        const welcomeGuide = document.querySelector('.edit-post-welcome-guide');
+        if (welcomeGuide) {
+            const closeButton = welcomeGuide.querySelector('.components-modal__header button');
+            if (closeButton) {
+                closeButton.click();
+                /*
+                 * Also add a class to body so we don't do it again - subsequent modals
+                 * will need to be shown as they will have been user-initiated.
+                 */
+                document.body.classList.add('ta-killed-post-welcome-guide');
+            }
+        }
+    }
 }
